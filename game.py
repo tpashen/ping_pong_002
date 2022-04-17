53ca7188d6b220d43a21c5e3474264cdba9ea572
bbfai1 <103671563+bbfai1@users.noreply.github.com>
2022-04-17 13:21:28 +0300

    Update game.py

--- game.py
+++ game.py
@@ -15,5 +15,16 @@ class GameSprite(sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 #2
 #3
-#4
+while game:
+    for e in event.get():
+        game = False
+    if finish != True:
+        window.fill(back)
+        racket1.update_1()
+        racket2.update_r()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(racket1,ball) or sprite.collide_rect(racket2, ball):
+            speed_x *= -1
+            speed_y *= 1
 #5
