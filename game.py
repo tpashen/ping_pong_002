d8550adf73c3fc5a7e77ad9481b5914af446345b
galamyt <75381109+galamyt@users.noreply.github.com>
2022-04-21 20:34:57 +0300

    Update game.py

--- game.py
+++ game.py
@@ -28,6 +28,25 @@ class Player(GameSprite):
         if keys[K_s] and self.rect.y < win_height - 80:
             self.rect.y += self.speed
 #3
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+window.fill(back)
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font('calabri.ttf', 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 #4
 while game:
     for e in event.get():
