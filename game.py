70033efe74ff854b214c9ac222e7e6c5e0f4a1ef
galamyt <75381109+galamyt@users.noreply.github.com>
2022-04-21 21:04:52 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,3 @@
-#1 
 from pygame import *
 
 class GameSprite(sprite.Sprite):
@@ -8,15 +7,15 @@ class GameSprite(sprite.Sprite):
         self.speed = player_speed
 
         self.rect = self.image.get_rect()
-        self.rect.x = player
+        self.rect.x = player_x
         self.rect.y = player_y
     
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 #2
 class Player(GameSprite):
-    def update_r():
-        key = key.get_pressed()
+    def update_r(self):
+        keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < win_height - 80:
@@ -40,20 +39,24 @@ clock = time.Clock()
 FPS = 60
 
 racket1 = Player('racket.png', 30, 200, 4, 50, 150)
-racket2 = player('racket.png', 520, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
 ball = GameSprite('tenis_ball.png', 200, 200, 4, 50, 50)
 
 font.init()
-font = font.Font('calabri.ttf', 35)
+font = font.Font('calibri.ttf', 35)
 lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
 lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
 #4
 while game:
     for e in event.get():
-        game = False
+        if e.type == QUIT:
+            game = False
     if finish != True:
         window.fill(back)
-        racket1.update_1()
+        racket1.update_l()
         racket2.update_r()
         ball.rect.x += speed_x
         ball.rect.y += speed_y
@@ -64,7 +67,7 @@ while game:
         if ball.rect.y > win_height-50 or ball.rect.y < 0:
             speed_y *= -1
 
-        if ball.reck.x < 0:
+        if ball.rect.x < 0:
             finish = True
             window.blit(lose1, (200, 200))
             game_over = True
